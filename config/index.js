9f5541ffab158f9f8e3beea4df140a5f49094b69
chenyu-0114 <113593388+chenyu-0114@users.noreply.github.com>
2022-09-15 20:25:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "43eeea08d759cb160dbf04316bb84b84",
+  APP_SECRET: "84b8d0c5aebd09137d3b53e7db9accad",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
